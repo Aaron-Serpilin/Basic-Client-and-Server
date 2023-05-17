776fd3ac0ebe959bbea82cf5e6a7df1e470e3ff4
Aaron-Serpilin <serpilin.aaron@gmail.com>
2023-05-17 17:23:09 +0200

    Commit to keep both our codes up to date

--- server.py
+++ server.py
@@ -1,79 +1,92 @@
 import socket
-import threading #Threads allow us to separate code out so that it is not waiting for other code to finish before it is able to execute
+import threading
 
 HEADER = 64
-PORT = 5050 #Course port is 5378
-SERVER = socket.gethostbyname(socket.gethostname()) #This automatically gets the IP address of every user/device. Course IP is 143.47.184.219
-ADDR = (SERVER, PORT) #The address is always a tuple
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
-DISCONNECT_MESSAGE = "Disconnect"
 user_list = {}
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Here we make a socket that allows the device to open up to other sockets
-server.bind(ADDR) #Anything that connects to the given address will use the corresponding socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
 
 def handle_client(conn, addr):
-    print(f"[New Connection] {addr} connected") #Prints which new connection was just established
 
-    connected = True
-    
-    while connected:
+    print(f"[New Connection] {addr} connected")
+    username = None
+    try:
 
-        data = ''
-        byte = ''
+        connected = True
 
-        try:
-            while byte != '\n': #Allows us to read byte-by-byte
+        while connected:
+
+            data = ''
+            byte = ''
+            username = ''
+
+            # while True:
+            #     byte = conn.recv(1).decode(FORMAT)
+            #     if not data:
+            #         raise Exception('User has thrown exception')
+                    
+            #     if data == '\n':
+            #         break
+            #     else:
+            #         byte += data
+                
+            while byte != '\n':
                 byte = conn.recv(1).decode(FORMAT)
                 data += byte
-        except Exception in e:
-            print("There was an error with the user. The error is {e}")
-            conn.close()
 
-        if data == '':
-            break
+            if not data:
+                raise Exception('client bye bye')
 
-        #print(f"{data}")
+            space_index = data.find(" ")
+            newline_index = data.find("\n")
 
-        space_index = data.find(" ")
-        newline_index = data.find("\n")
-    
-        if data[0:space_index] == "HELLO-FROM":
-            username = data[space_index + 1:newline_index]
-            print(f"Username is: {username}")
-            msg = bytes(f"HELLO {data[space_index:]}\n", FORMAT) #For handshake, the message is sent in byte form, which is why decoding is required
-            #print(msg)
-            conn.send(msg)
-
-            if username in user_list:
-                msg = bytes(f"IN USE\n", FORMAT)
+            if data[0:space_index] == "HELLO-FROM":
+                username = data[space_index + 1:newline_index]
+                logged_user = username
+                print(f"Username is: {username}")
+                msg = bytes(f"HELLO {data[space_index:]}\n", FORMAT)
                 conn.send(msg)
-                break
-            else:
-                user_list[username] = conn #We add the username as well as the connection so we can relate the logged in users appropriately
-        elif data[0:space_index] == "LIST":
-            user_list_message = str()
-            for key in user_list:
-                user_list_message += key + ","
-            msg = bytes(f"LIST-OK {user_list_message}\n", FORMAT)
-            conn.send(msg)
+
+                if username in user_list:
+                    msg = bytes(f"IN USE\n", FORMAT)
+                    conn.send(msg)
+                    break
+                else:
+                    user_list[username] = conn
+            elif data[0:space_index] == "LIST":
+                user_list_message = str()
+                for key in user_list:
+                    user_list_message += key + ","
+                msg = bytes(f"LIST-OK {user_list_message}\n", FORMAT)
+                conn.send(msg)
     
-    conn.close() #We close the connection 
+    except Exception as e:  
+            print(f"There was an error with the user. The error is {e}")
+
+    finally:
+        if username and username in user_list:
+            del user_list[username]
+            print(f"This user has been deleted: {username}")
+            
+
+    conn.close()
+
 
 def start():
-
-    server.listen() #We listen for new connections
+    server.listen()
     print(f"Server is listening on {SERVER}")
 
     while True:
-        conn, addr = server.accept() #Every time a new connection is established, a new object with the address is created
-        thread = threading.Thread(target = handle_client, args = (conn, addr)) #Every time a new connection is done, we thread and pass it to the handle_client function
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print(f"[Active Connections] {threading.active_count() - 1}") #Prints amount of currently active threads/clients
+        print(f"[Active Connections] {threading.active_count() - 1}")
 
 
 print("Server has been started")
-start ()
-
-
-
+start()
